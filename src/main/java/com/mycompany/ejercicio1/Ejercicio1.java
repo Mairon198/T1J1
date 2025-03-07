f06105625f29284e1024ac15211aec3ca88188ee
Mairon198 <omarlicona198@gmail.com>
2025-03-06 20:48:46 -0600

    Agregando Tarea No. 1, Ejercicio No. 1

---
+++ src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
@@ -0,0 +1,15 @@
+//Mairon Omar Licona Duarte
+//2018100200
+//Crear el código que imprima la siguiente salida: "Hola, soy Arnol Gutiérrez" (Usar su nombre)
+
+package com.mycompany.ejercicio1;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hola, soy Mairon Licona");
+    }
+}
+    
+
